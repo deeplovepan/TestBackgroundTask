d306cc696606476bbd1a441630876b677336c98d
Peter Pan <deeplovepan@gmail.com>
2013-07-18 15:13:52 +0800

    Initial Commit

---
+++ TestBackgroundTaskTests/TestBackgroundTaskTests.m
@@ -0,0 +1,32 @@
+//
+//  TestBackgroundTaskTests.m
+//  TestBackgroundTaskTests
+//
+//  Created by Pan Peter on 13/7/18.
+//  Copyright (c) 2013年 Pan Peter. All rights reserved.
+//
+
+#import "TestBackgroundTaskTests.h"
+
+@implementation TestBackgroundTaskTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TestBackgroundTaskTests");
+}
+
+@end
